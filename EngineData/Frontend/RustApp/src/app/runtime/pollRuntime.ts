83faa2a76a6b62d3f12222b4d5e3f60cd575d064
MIVUBI-STD <mivubiteam@gmail.com>
2026-09-22 01:45:01 +0700

    cleanup: remove obsolete generic frontend poll helper

--- EngineData/Frontend/RustApp/src/app/runtime/pollRuntime.ts
+++
@@ -1,8 +0,0 @@
-export function startRuntimePoll(
-  callback: () => void | Promise<void>,
-  intervalMs: number,
-): () => void {
-  void callback();
-  const timer = window.setInterval(() => void callback(), intervalMs);
-  return () => window.clearInterval(timer);
-}
